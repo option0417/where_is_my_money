0a644bbd130ec8f2ac264764ba1143c3a4f95a9d
option0417 <you@example.com>
2017-05-24 17:31:45 +0000

    1. Rename index.js to main.js. 2. Add headers for CORS issue

--- main.js
+++ main.js
@@ -5,12 +5,28 @@ const bodyParser= require('body-parser')
 api.use(bodyParser.urlencoded({extended: true}))
 
 api.get('/record', function (req, res) {
+	res.set(
+		{
+			'Access-Control-Allow-Origin' : req.get('Origin')
+		}
+	);
+
   res.send('Hello World!')
 });
 
 api.post('/record', function(req, res) {
   console.log(req.body)
 });
+
+api.options('/record',  function(req,  res) {
+	console.log(req);
+	res.set(
+		{
+			'Access-Control-Allow-Origin' : '*'
+		}
+	);
+	res.sendStatus(200);
+})
 
 api.listen(3000, function () {
   console.log('Example app listening on port 3000!')
