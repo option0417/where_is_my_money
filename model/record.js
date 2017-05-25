46bfcfb8055af472aef3e60418f1156bb8a9cc18
option0417 <you@example.com>
2017-05-25 02:57:02 +0000

    Re-name file

--- model/record.js
+++
@@ -1,8 +0,0 @@
-function Record(record_id, record_type, item_name, item_price, amount, cost) {
-  this.record_id 	= record_id;
-  this.record_type 	= record_type;
-  this.item_name 	= item_name;
-  this.item_price 	= item_price;
-  this.amount 		= amount;
-  this.cost 		= cost;
-}
